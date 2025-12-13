7f5b3744905025fb9e13ae927679a4b4cf1c72c1
Martin Dahlgren <mdahlgrengadd@users.noreply.github.com>
2025-12-13 12:19:46 +0100

    Authentication using FastAPI OAuth2 example

---
+++ src/Artist.py
@@ -0,0 +1,12 @@
+from RegisteredUser import RegisteredUser
+
+
+class Artist(RegisteredUser):
+    def upload_song(self):
+        pass
+
+    def upload_podcast(self):
+        pass
+
+    def manage_album(self):
+        pass
